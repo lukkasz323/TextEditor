a65bbace061f74a8d67b10692cf9f2c6105c6178
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2024-04-29 14:18:27 +0200

    Move editor init to it's own class

--- TextEditor/Scene/Editor.cs
+++ TextEditor/Scene/Editor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
 using System.Text;
 
@@ -17,5 +18,20 @@ internal class Editor
             new(spriteFont, "Edit"),
             new(spriteFont, "About"),
         };
+        RelocateDropdowns();
+
+        void RelocateDropdowns()
+        {
+            Point origin = new(8, 3);
+            int textSpacing = 8;
+            int widthSpacing = 8;
+            int widthOffset = 0;
+            foreach (Button button in Dropdowns)
+            {
+                button.Body = new Rectangle(widthOffset + origin.X, button.Body.Y + origin.Y, (int)button.TextSize.X + widthSpacing, (int)button.TextSize.Y);
+
+                widthOffset += (int)button.TextSize.X + textSpacing + widthSpacing;
+            }
+        }
     }
 }
